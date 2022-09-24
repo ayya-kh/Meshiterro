2dab4fd28bb3a7f535edd6fca57986b6aaeda5d9
ayya-kh <76466532+ayya-kh@users.noreply.github.com>
2022-09-24 04:31:58 +0000

    [Finish] 4-7

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,22 @@
+class ApplicationController < ActionController::Base
+  
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+  #after_sign_in_path_forはDeviseが用意しているメソッドで、サインイン後にどこに遷移するかを設定しているメソッド
+  #Aboutページへ遷移するように設定
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+  
+  #after_sign_out_path_forはDeviseが用意しているメソッドでサインアウト後にどこに遷移するかを設定するメソッド
+  #Aboutページへ遷移するように設定
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+end
