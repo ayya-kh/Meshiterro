409c60045cf7a225db48ba108a010d15f22f2220
ayya-kh <76466532+ayya-kh@users.noreply.github.com>
2022-09-24 14:16:22 +0000

    [Finish] 4-12

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,4 +2,12 @@ class PostImage < ApplicationRecord
   has_one_attached :image
   #1:複数 の「複数」側にあたるモデルに、belongs_to を記載する必要がある
   belongs_to :user
+  
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
 end
